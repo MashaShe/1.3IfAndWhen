786bf0c48e9183e6bf4af46df9a3d01d2c2a15b6
MashaShe <shabalkova@gmail.com>
2020-12-11 09:12:38 +0300

    Upated with in and different percantage calculation

--- src/main/kotlin/1.1LastSeen.kt
+++ src/main/kotlin/1.1LastSeen.kt
@@ -1,21 +1,21 @@
 
 fun agoToText (secondsNotOnline:Int){
     when{
-        secondsNotOnline<61 -> println("был(а) только что\n")
-        secondsNotOnline<60*60 -> correctTextMin(secondsNotOnline/60)
-        secondsNotOnline<24*60*60 ->correctTextHours(secondsNotOnline/60/60)
-        secondsNotOnline<24*60*60*2 -> println("был(а) в сети сегодня\n")
-        secondsNotOnline<24*60*60*3 -> println("был(а) в сети вчера\n")
+        secondsNotOnline in 0..60 -> println("был(а) только что\n")
+        secondsNotOnline in 61..60*60 -> correctTextMin(secondsNotOnline/60)
+        secondsNotOnline in (60*60+1)..24*60*60 ->correctTextHours(secondsNotOnline/60/60)
+        secondsNotOnline in (24*60*60+1)..24*60*60*2 -> println("был(а) в сети сегодня\n")
+        secondsNotOnline in (24*60*60*2+1)..24*60*60*3 -> println("был(а) в сети вчера\n")
         secondsNotOnline>24*60*60*3 -> println("был(а) в сети давно\n")
     }
 }
 
 fun correctTextHours(hoursNotOnline: Int) {
     when {
-        hoursNotOnline<2 -> println("был(а) в сети ${hoursNotOnline} час назад\n")
-        hoursNotOnline<5 -> println("был(а) в сети ${hoursNotOnline} часа назад\n")
-        hoursNotOnline<20 -> println("был(а) в сети ${hoursNotOnline} часов назад\n")
-        hoursNotOnline<22 -> println("был(а) в сети ${hoursNotOnline} час назад\n")
+        hoursNotOnline in 0..1 -> println("был(а) в сети ${hoursNotOnline} час назад\n")
+        hoursNotOnline in 2..4 -> println("был(а) в сети ${hoursNotOnline} часа назад\n")
+        hoursNotOnline in 5..20 -> println("был(а) в сети ${hoursNotOnline} часов назад\n")
+        hoursNotOnline in 21..22 -> println("был(а) в сети ${hoursNotOnline} час назад\n")
         hoursNotOnline<=24 -> println("был(а) в сети ${hoursNotOnline} часа назад\n")
     }
 }
--- src/main/kotlin/1.2Comission.kt
+++ src/main/kotlin/1.2Comission.kt
@@ -19,11 +19,11 @@ fun comissioncalc(cardType: String = "VK Pay",
 }
 
 fun visaComission(monthSum: Int, transferSum: Int): Int {
-    if (transferSum / 100 * 0.75 < 35 * 100) return 35 * 100
-    else return (transferSum / 100 * 0.75).roundToInt()
+    if (transferSum * 0.0075 < 35 * 100) return 35 * 100
+    else return (transferSum * 0.0075).roundToInt()
 }
 
 fun masterComission(monthSum: Int, transferSum: Int): Int {
     if (monthSum < 75000 * 100) return 0
-    else return (transferSum / 100 * 0.6 + 20 * 100).roundToInt()
+    else return (transferSum * 0.006 + 20 * 100).roundToInt()
 }
